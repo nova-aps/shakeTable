d15794e7328b4b286a2f2bb5b7fd7f011537e3f1
Enrico Calore <enrico.calore@fe.infn.it>
2023-09-19 15:31:11 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shakeTable
 Simple code to control the shake table servos 
+
+Quick and dirty implementation controlling a single servo developed during the 2023 HIMS festival.
---
+++ single-servo/single-servo.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>
+
+
+Servo myservo;  // create servo object to control a servo
+
+// twelve servo objects can be created on most boards
+
+
+int pos = 90;    // variable to store the servo position
+
+
+void setup() {
+
+
+  pinMode(0, INPUT_PULLUP);
+
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+}
+
+
+void loop() {
+
+ if (!digitalRead(0)) {
+
+  for (pos = 90; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
+
+    // in steps of 1 degree
+
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+
+    delay(15);                       // waits 15ms for the servo to reach the position
+
+  }
+
+  for (pos = 180; pos >= 90; pos -= 5) { // goes from 180 degrees to 0 degrees
+
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+
+    delay(15);                       // waits 15ms for the servo to reach the position
+
+  }
+
+ }
+
+}
